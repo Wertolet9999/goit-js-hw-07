84f60b5d5387d90456aa0ee935b6c843ea6f3b24
Wertolet9999 <wertolet9999@gmail.com>
2022-12-12 19:19:58 +0200

    add task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = makeGallery(galleryItems);
+galleryEl.insertAdjacentHTML('afterbegin', galleryMarkup);
+galleryEl.addEventListener('click', selectImage);
+
+function makeGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+              <a class="gallery__link" href="${original}">
+                  <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+              </a>
+            </div>`;
+    }).join('');
+}
+function selectImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+      return
+  }
+    }
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
